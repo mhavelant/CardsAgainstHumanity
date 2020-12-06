c97788f51ffac21c33c0eb89c30fbe30d661ebb0
mhavelant <mhavelant@brainsum.com>
2020-12-06 22:09:40 +0100

    Make assets work in prod mode.

--- app/src/main.go
+++ app/src/main.go
@@ -6,22 +6,41 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"time"
 )
 
-var tpl = template.Must(template.ParseFiles("templates/index.html"))
+var binDir = currentDir()
+var tpl = template.Must(template.ParseFiles(binDir+"/templates/index.html"))
+
+func currentDir() (here string) {
+	// @todo: Only for testing, should decouple it later. Maybe try using vue.js?
+	if os.Getenv("APP_MODE") == "dev" {
+		here = "."
+		return
+	}
+
+	here = path.Dir(os.Args[0])
+
+	if !path.IsAbs(os.Args[0]) {
+		workdir, _ := os.Getwd()
+		here = path.Join(workdir, here)
+	}
+
+	return
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	_ = tpl.Execute(w, nil)
 }
 
 func main() {
+	log.Println("Current directory/Workdir: "+currentDir())
 	r := mux.NewRouter()
 
-	// @todo: Only for testing, should decouple it later. Maybe try using vue.js?
 	r.HandleFunc("/", indexHandler)
 
-	publicFs := http.FileServer(http.Dir("assets"))
+	publicFs := http.FileServer(http.Dir(binDir+"/assets"))
 	r.PathPrefix("/css/").Handler(publicFs)
 	r.PathPrefix("/js/").Handler(publicFs)
 	r.PathPrefix("/ico/").Handler(publicFs)
